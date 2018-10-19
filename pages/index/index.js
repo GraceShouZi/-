dc244b96f4878ae192be17faf8263032643947a1
unknown <sunxiao@ad.yxcps.com>
2018-10-19 11:18:22 +0800

    first commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,13 @@ var QQMapWX = require('../../utils/qqmap-wx-jssdk.min.js');
 var qqmapsdk;
 Page({
   data: {
-    address:"获取位置",
-    src:""
+    address:"定位中..."
   },
   onLoad: function () {
     var that = this;
     setTimeout(function(){
       that.listenerBtnGetLocation();
-    },1000)
+    },1000);
   },
   listenerBtnGetLocation:function(){
     var that = this;
@@ -23,11 +22,10 @@ Page({
       success: function (res) {
         var title = res.result.address_reference.landmark_l2.title;
         that.setData({
-          address: title
+          address: res.result.address
         });
       },
       fail: function (res) {
-        console.log(res);
         that.setData({
           address: '定位失败'
         });
@@ -39,16 +37,38 @@ Page({
     var that = this;
     wx.chooseLocation({
       success: function (res) {
-        that.setData({
-          address: res.name
-        });
+        console.log(res)
+        if (res.name !="" ){
+          that.setData({
+            address: res.name
+          });
+        }
       },
       fail: function (err) {
         console.log(err)
       }
     });
-  }
+  },
+  onShareAppMessage: function (ops) {
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+    }
+    return {
+      title: 'xx小程序',
+      path: 'pages/index/index',
+      imageUrl:'https://img3.doubanio.com/view/note/sqs/public/p54638813.jpg',
+      success: function (res) {
+        // 转发成功
+        console.log("转发成功:" + JSON.stringify(res));
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
 
+  }
 
 })
 
